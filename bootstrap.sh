0abf160820b3f26a9f767d26fd9db555191ab219
Stefan Moraru <stefan@stefanmoraru.ro>
2024-10-02 16:04:51 +0300

    Remove unused package. Add zsh-autosuggestions install step

--- bootstrap.sh
+++ bootstrap.sh
@@ -69,10 +69,12 @@ brew install bat
 brew install zoxide
 brew install fzf
 brew install git-delta
-brew install thefuck
 
 ## Configs
 echo "Installing custom configs.."
+
+### zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 ### zsh powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
