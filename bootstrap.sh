7126403a8681ae5fb55c6b52e1b71ccc7400815f
Stefan Moraru <stefan@stefanmoraru.ro>
2025-01-16 12:07:28 +0200

    feat(macos): Replace manual steps with commands

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,17 +4,11 @@
 echo "You need to manually install:"
 
 echo "Docker Desktop: https://www.docker.com/products/docker-desktop/"
-
 echo "Arc: https://arc.net/"
 
 read -p "Did you install the packages above? [y/n] " RESP
 
 ### Manual settings
-echo "[macos] Settings -> Appearance -> Dark mode"
-echo "[macos] Settings -> Trackpad -> Enable Tap to click"
-echo "[macos] Settings -> Trackpad -> Secondary click = Click in Bottom Right Corner"
-echo "[macos] Settings -> Trackpad -> Disable Natural scrolling"
-
 echo "[arc] Settings -> Shortcuts -> Remove 'New Window' shortcut"
 echo "[arc] Settings -> Shortcuts -> Remove 'New Little Arc Window' shortcut"
 
@@ -69,4 +63,49 @@ cp ./config/git/gitconfig_personal ~/projects/.gitconfig_personal
 cp ./config/nvim/init.lua ~/.config/nvim/
 
 ### mac specific
+
+### Close System settings to prevent overrides
+osascript -e 'tell application "System Preferences" to quit'
+
+#### Dark mode
+defaults write NSGlobalDomain AppleInterfaceStyle Dark
+
+#### Enable tap to click
+defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+#### Right corner right click
+defaults write com.apple.AppleMultitouchTrackpad TrackpadCornerSecondaryClick -int 2
+defaults write com.apple.AppleMultitouchTrackpad TrackpadRightClick -bool true
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadCornerSecondaryClick -int 2
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadRightClick -bool true
+defaults -currentHost write NSGlobalDomain com.apple.trackpad.trackpadCornerClickBehavior -int 1
+
+#### Disable natural scrolling
+defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
+
+#### Minimize to application
+defaults write com.apple.dock minimize-to-application -bool true
+
+#### Do not show recents in Dock
+defaults write com.apple.dock show-recents -bool false
+
+#### Disable key hold
 defaults write -g ApplePressAndHoldEnabled -bool false
+
+#### Disable text correction
+defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+defaults write NSGlobalDomain NSAutomaticTextCompletionEnabled -bool false
+
+#### Finder
+defaults write com.apple.finder ShowPathbar -bool true
+defaults write com.apple.finder ShowStatusBar -bool true
+defaults write com.apple.finder FXPreferredViewStyle Nlsv
+defaults write com.apple.finder QuitMenuItem -bool true
