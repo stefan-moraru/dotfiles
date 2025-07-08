f5b2e9b653f805004d19133334bf011723cab806
Stefan Moraru <stefan@stefanmoraru.ro>
2025-07-08 12:23:41 +0300

    fix(nvim): align text eol to prevent it from overlapping

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -85,7 +85,7 @@ require("lazy").setup({
 			current_line_blame = true,
 			current_line_blame_opts = {
 				virt_text = true,
-				virt_text_pos = "right_align",
+				virt_text_pos = "eol",
 				delay = 10,
 				ignore_whitespace = true,
 				virt_text_priority = 100,
